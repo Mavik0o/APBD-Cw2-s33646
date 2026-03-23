202dc6d7fae791500be105f69f210841add7ce50
Łukasz Dziubiela <s33646@pjwstk.edu.pl>
2026-03-23 20:04:55 +0100

    Chyba service dwie klasy zrobione, nie pamietam co zrobilem po przerwie

--- UczelnianaWypozyczalniaSprzetuProjekt/Services/Equipment/EquipmentService.cs
+++ UczelnianaWypozyczalniaSprzetuProjekt/Services/Equipment/EquipmentService.cs
@@ -1,6 +1,37 @@
-﻿namespace UczelnianaWypozyczalniaSprzetuProjekt.Services.Equipment;
+﻿using UczelnianaWypozyczalniaSprzetuProjekt.Enums;
+using UczelnianaWypozyczalniaSprzetuProjekt.Exceptions;
+using UczelnianaWypozyczalniaSprzetuProjekt.Models;
+
+namespace UczelnianaWypozyczalniaSprzetuProjekt.Services.Equipment;
 
 public class EquipmentService
 {
-    
+    private readonly List<Przedmioty> _equipment = [];
+
+    public void AddEquipment(Przedmioty equipment)
+    {
+        _equipment.Add(equipment);
+    }
+
+    public Przedmioty GetById(int id)
+    {
+        return _equipment.FirstOrDefault(e => e.Id == id)
+               ?? throw new EquipmentNotFoundException(id);
+    }
+
+    public List<Przedmioty> GetAll()
+    {
+        return _equipment;
+    }
+
+    public List<Przedmioty> GetAvailable()
+    {
+        return _equipment.Where(e => e.Status == StatusPrzedmiotow.Available).ToList();
+    }
+
+    public void MarkUnavailable(int id)
+    {
+        var equipment = GetById(id);
+        equipment.Status = StatusPrzedmiotow.Unavailable;
+    }
 }
--- UczelnianaWypozyczalniaSprzetuProjekt/Services/Users/UsersService.cs
+++ UczelnianaWypozyczalniaSprzetuProjekt/Services/Users/UsersService.cs
@@ -1,6 +1,25 @@
-﻿namespace UczelnianaWypozyczalniaSprzetuProjekt.Services.Users;
+﻿using UczelnianaWypozyczalniaSprzetuProjekt.Exceptions;
+using UczelnianaWypozyczalniaSprzetuProjekt.Models;
+
+namespace UczelnianaWypozyczalniaSprzetuProjekt.Services.Users;
 
 public class UsersService
 {
-    
+    private readonly List<Uzytkownik> _users = [];
+
+    public void AddUser(Uzytkownik user)
+    {
+        _users.Add(user);
+    }
+
+    public Uzytkownik GetById(int id)
+    {
+        return _users.FirstOrDefault(u => u.Id == id)
+               ?? throw new UserNotFoundException(id);
+    }
+
+    public List<Uzytkownik> GetAll()
+    {
+        return _users;
+    }
 }
