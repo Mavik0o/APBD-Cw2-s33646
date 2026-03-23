62da954ae7efbfe8664eadca44bf6f7a5cf9985c
Łukasz Dziubiela <s33646@pjwstk.edu.pl>
2026-03-23 19:01:25 +0100

    Klasa kamera zrobiona

--- UczelnianaWypozyczalniaSprzetuProjekt/Models/Kamera.cs
+++ UczelnianaWypozyczalniaSprzetuProjekt/Models/Kamera.cs
@@ -1,6 +1,18 @@
 ﻿namespace UczelnianaWypozyczalniaSprzetuProjekt.Models;
 
-public class Kamera
+public class Kamera : Przedmioty
 {
-    
+    public int ResolutionMp { get; set; }
+    public bool IsDslr { get; set; }
+
+    public Kamera(string name, int resolutionMp, bool isDslr) : base(name)
+    {
+        ResolutionMp = resolutionMp;
+        IsDslr = isDslr;
+    }
+
+    public override string ToString()
+    {
+        return $"{base.ToString()}, Type: Camera, Resolution: {ResolutionMp} MP, DSLR: {IsDslr}";
+    }
 }
